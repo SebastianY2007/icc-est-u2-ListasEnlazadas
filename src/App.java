63b607c68894ec762d23620370e8ea78aa44f652
Sebastian <derlis567y@gmail.com>
2025-06-11 08:59:31 -0500

    Terminado

--- src/App.java
+++ src/App.java
@@ -1,8 +1,19 @@
-import Controller.MenuController;
+import Model.Contact;
 
 public class App {
     public static void main(String[] args) {
-        MenuController menuController = new MenuController();
-        menuController.showMenu();
+        // MenuController menuController = new MenuController();
+        // menuController.showMenu();
+
+        Contact c1 = new Contact("Juan", "123");
+        Contact c4 = new Contact("Juan");
+        Contact c2 = new Contact("Juan", null);
+        Contact c3 = new Contact("Juana", "456");
+        Contact c5 = new Contact("Juan", "123");
+
+        System.out.println(c1.equals (c2));
+        System.out.println(c1.equals (c3));
+        System.out.println(c1.equals (c4));
+        System.out.println(c1.equals (c5));
     }
 }
--- src/Model/Contact.java
+++ src/Model/Contact.java
@@ -3,17 +3,50 @@ package Model;
 public class Contact<T, U> {
     private T name;
     private U phone;
-    
+
+    public Contact() {
+    }
+
+    public Contact(T name) {
+        this.name = name;
+    }
+
     public Contact(T name, U phone) {
         this.name = name;
         this.phone = phone;
     }
+
     public T getName() {
         return name;
     }
+
     public U getPhone() {
         return phone;
     }
+
+    @Override
+    public boolean equals (Object obj){
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()){
+            return false;
+        }
+
+        // Aqui obj y this son clase contacto
+
+        Contact c = (Contact) obj;
+        return name != null &&  name.equals(c.getName());
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+        return result;
+    }
+
     @Override
     public String toString() {
         return name + " - " + phone;
